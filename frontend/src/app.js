ed5efee1d929dbd83496419533637faae1f91f56
bhupeshko <73344171+bhupeshko@users.noreply.github.com>
2026-03-23 19:05:55 +0530

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Scoreboard from "./Scoreboard";
+
+function App() {
+  return (
+    <div>
+      <h1>🏏 Cricket Scoring App</h1>
+      <Scoreboard />
+    </div>
+  );
+}
+
+export default App;
